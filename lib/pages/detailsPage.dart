39c5599316f1bd028ab47bb42ddf35f118bf990d
Eya Arfa <arfaaya0@gmail.com>
2023-03-03 00:01:10 +0100

    detailsPageV0.3

--- lib/pages/detailsPage.dart
+++ lib/pages/detailsPage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -7,70 +9,136 @@ class detailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var img = ['halal food', 'bio food', 'gluten free'];
-    return SafeArea(
-        child: Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(children: [
         ListView(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5),
           shrinkWrap: true,
           children: [
             Image.network(
               "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0n69IGWG0EJo6Q-y5_hlxrQkcrslN_R_2Gaf6Tbl1J7tq6xw57n7CX5aL0VvE9EUuFtU&usqp=CAU",
             ),
-            Text(
+            const Text(
               "Fresh Meat",
-              style: TextStyle(fontFamily: "Falling", fontSize: 35),
+              style: TextStyle(
+                fontFamily: "Falling",
+                fontSize: 35,
+                color: Color(0xff123153),
+              ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
             Container(
               height: 100,
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
-                  color: Color.fromARGB(255, 243, 243, 243)),
-              padding: EdgeInsets.all(10),
+                  color: const Color.fromARGB(255, 243, 243, 243)),
               child: Center(
                 child: ListView(
-                    physics: NeverScrollableScrollPhysics(),
+                    physics: const NeverScrollableScrollPhysics(),
                     scrollDirection: Axis.horizontal,
-                    // itemCount: img.length,
                     shrinkWrap: true,
-                    children: img.map((e) {
-                      return Attribute(atrributeName: e);
+                    children: img.asMap().entries.map((e) {
+                      return Attribute(
+                        atrributeName: e.value,
+                        index: e.key,
+                      );
                     }).toList()),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
-            Text(
+            const Text(
               'Details',
-              style: TextStyle(fontFamily: "Falling", fontSize: 35),
+              style: TextStyle(
+                fontFamily: "Falling",
+                fontSize: 35,
+                color: Color(0xff123153),
+              ),
             ),
-            Expanded(
-              child: Text(
-                'some further inormationhhhhhhh\n'
-                'fgdfgdfghgfhgnhgfhfghjfjhfjgj'
-                'fgdfgdfghgfhgnhgfhfghjfjhfjgj\n'
-                'fgdfgdfghgfhgnhgfhfghjfjhfjgj\nfgdfgdfghgfhgnhgfhfghjfjhfjgj\ndgsfghksdjhfkjdhfdkjsbfkjxczxncxjc\nuhsdfhjsdcjhkhc\n',
-                style: TextStyle(fontSize: 15),
+            const Text(
+              'some further inormationhhhhhhh\n'
+              'fgdfgdfghgfhgnh\ngfhfghjfjhfjgj'
+              'fgdfgdfghgfhgnhgfhfghjfjhfjgj\n'
+              'fgdfgdfghgfhg\nlklklklklklklkl\nkjkjijjijijihihi\nnhgfhfghjfjhfjgj\n'
+              'fgdfgdfghgfhgnhgfhfghjfjhfjgj\n',
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.black54,
               ),
             ),
           ],
         ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            width: MediaQuery.of(context).size.width,
+            height: 80,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: const Color.fromARGB(255, 255, 255, 255)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "Price",
+                      style: TextStyle(
+                        color: Colors.black26,
+                        fontSize: 20,
+                        fontFamily: "Falling",
+                      ),
+                    ),
+                    Text("\$data",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontFamily: "Falling",
+                          color: Color(0xff123153),
+                        )),
+                  ],
+                ),
+                TextButton(
+                    onPressed: () => {},
+                    style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                        ),
+                        backgroundColor:
+                            MaterialStateProperty.all(const Color(0xff56e39f)),
+                        padding: MaterialStateProperty.all(
+                            const EdgeInsets.symmetric(
+                                horizontal: 50, vertical: 15))),
+                    child: const Text(
+                      "Add to Cart",
+                      style: TextStyle(
+                          color: Color(0xff123153),
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Falling"),
+                    )),
+              ],
+            ),
+          ),
+        )
       ]),
-    ));
+    );
   }
 }
 
 class Attribute extends StatelessWidget {
   String atrributeName;
-  Attribute({
-    Key? key,
-    required this.atrributeName,
-  }) : super(key: key);
+  int index;
+  static const colors = [0xffc7f4c5, 0xfff3f3c2, 0xffc4f1f5];
+  Attribute({Key? key, required this.atrributeName, required this.index})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -79,13 +147,20 @@ class Attribute extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          ImageIcon(
-            AssetImage('assets/images/' + atrributeName + '.png'),
-            size: 30,
+          CircleAvatar(
+            backgroundColor: Color(colors[index]),
+            child: ImageIcon(
+              AssetImage('assets/images/' + atrributeName + '.png'),
+              size: 30,
+            ),
           ),
           Text(
             atrributeName[0].toUpperCase() + atrributeName.substring(1),
-            style: TextStyle(fontFamily: 'Falling', fontSize: 20),
+            style: const TextStyle(
+              fontFamily: 'Falling',
+              fontSize: 20,
+              color: Color(0xff123153),
+            ),
           )
         ],
       ),
